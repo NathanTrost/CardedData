56a5e72a9e32a7df9eabe7ac82eaeff4abf6167e
Nathan Trost <n.d.trost@gmail.com>
2020-09-26 17:19:35 -0500

    Test on sort function

--- src/utils/sortMethods.js
+++ src/utils/sortMethods.js
@@ -1,13 +1,21 @@
+import compareAsc from "date-fns/compareAsc";
+import compareDesc from "date-fns/compareDesc";
+import isValid from "date-fns/isValid";
+
 export const sortAscendingByKey = (array, key) =>
   array.sort((a, b) => {
-    if (a[key] < b[key]) return -1;
-    if (a[key] > b[key]) return 1;
+    const first = a[key];
+    const second = b[key];
+    if (first < second) return -1;
+    if (first > second) return 1;
     return 0;
   });
 
 export const sortDescendingByKey = (array, key) =>
   array.sort((a, b) => {
-    if (a[key] > b[key]) return -1;
-    if (a[key] < b[key]) return 1;
+    const first = a[key];
+    const second = b[key];
+    if (first > second) return -1;
+    if (first < second) return 1;
     return 0;
   });
--- src/utils/sortMethods.test.js
+++ src/utils/sortMethods.test.js
@@ -2,9 +2,27 @@ import React from "react";
 import { sortAscendingByKey, sortDescendingByKey } from "./sortMethods";
 
 const data = [
-  { firstName: "Mark", lastName: "Hamill", age: "25" },
-  { firstName: "Harrison", lastName: "Ford", age: "35" },
-  { firstName: "Carrie", lastName: "Fisher", age: "20" },
+  {
+    firstName: "Mark",
+    lastName: "Hamill",
+    age: "25",
+    dob: "09-25-1951",
+    dobLong: "September 25th, 1951",
+  },
+  {
+    firstName: "Harrison",
+    lastName: "Ford",
+    age: "35",
+    dob: "07-13-1942",
+    dobLong: "July 13th, 1942",
+  },
+  {
+    firstName: "Carrie",
+    lastName: "Fisher",
+    age: "20",
+    dob: "10-21-1956",
+    dobLong: "October 21st, 1956",
+  },
 ];
 
 describe("sortAscendingByKey: ", () => {
@@ -16,6 +34,24 @@ describe("sortAscendingByKey: ", () => {
     expect(newData[1].firstName).toBe("Harrison");
     expect(newData[2].firstName).toBe("Mark");
   });
+
+  test("should sort dates alphabetically from earliest to latest", () => {
+    const newData = sortAscendingByKey(data, "dob");
+
+    expect(newData).toHaveLength(3);
+    expect(newData[0].firstName).toBe("Harrison");
+    expect(newData[1].firstName).toBe("Mark");
+    expect(newData[2].firstName).toBe("Carrie");
+  });
+
+  //   test("should sort long dates non-alphabetically from earliest to latest", () => {
+  //     const newData = sortAscendingByKey(data, "dobLong");
+
+  //     expect(newData).toHaveLength(3);
+  //     expect(newData[0].firstName).toBe("Harrison");
+  //     expect(newData[1].firstName).toBe("Mark");
+  //     expect(newData[2].firstName).toBe("Carrie");
+  //   });
 });
 
 describe("sortDescendingByKey: ", () => {
@@ -27,4 +63,22 @@ describe("sortDescendingByKey: ", () => {
     expect(newData[1].lastName).toBe("Ford");
     expect(newData[2].lastName).toBe("Fisher");
   });
+
+  test("should sort dates alphabetically from latest to earliest", () => {
+    const newData = sortDescendingByKey(data, "dob");
+
+    expect(newData).toHaveLength(3);
+    expect(newData[0].firstName).toBe("Carrie");
+    expect(newData[1].firstName).toBe("Mark");
+    expect(newData[2].firstName).toBe("Harrison");
+  });
+
+  //   test("should sort long dates non-alphabetically from latest to earliest", () => {
+  //     const newData = sortDescendingByKey(data, "dob");
+
+  //     expect(newData).toHaveLength(3);
+  //     expect(newData[0].firstName).toBe("Carrie");
+  //     expect(newData[1].firstName).toBe("Mark");
+  //     expect(newData[2].firstName).toBe("Harrison");
+  //   });
 });
