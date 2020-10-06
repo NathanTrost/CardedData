ce53dfb5f7955c87b5b564e117a848e19d1f9184
Nathan Trost <n.d.trost@gmail.com>
2020-10-06 15:44:42 -0500

    Switch to emotion

--- src/components/styled/Grid.js
+++ src/components/styled/Grid.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 import { setRem, setShadow } from "./utils";
 
 const itemSpacing = "20px";
--- src/components/styled/utils.js
+++ src/components/styled/utils.js
@@ -1,4 +1,4 @@
-import { css } from "styled-components";
+import { css } from "@emotion/core";
 
 export const setColor = {
   primaryColor: "#0080FF",
