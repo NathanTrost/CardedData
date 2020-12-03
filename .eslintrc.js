547cf8c8ec5a9f1f48eb42e292f8a74c5e147e2a
Nathan Trost <n.d.trost@gmail.com>
2020-12-03 09:58:54 -0600

    Number within range prop type (#12)
    * Add custom numberWithinRange proptype
    
    * Update packages and eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,4 +12,11 @@ module.exports = {
     "jsx-a11y/anchor-is-valid": 0,
     "jsx-a11y/label-has-for": 0,
   },
+
+  overrides: [
+    {
+      files: ["**/*.stories.js"],
+      rules: { "import/no-anonymous-default-export": "off" },
+    },
+  ],
 };
